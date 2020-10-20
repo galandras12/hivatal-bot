25a9e1d5f64a0999a75f01b02db6b58064ddc119
Andrew Gal <galandras12@gmail.com>
2020-10-20 15:27:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,46 +2,14 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
 const prefix = "!";
-var Imap     = require('imap'),
-    inspect  = require('util').inspect;
-var fs       = require('fs');
 //require('log-timestamp')('HIVATAL-BOT:', 'Europe/Budapest');
 require('console-inject');
-
-
 	
 client.on('ready', () => {
   console.debug('BOT is RUN');
-  
 
  // client.channels.cache.get("760790573354844190").send("Hivatal BOT is `RESTARTED`");
-  
-/*  setInterval(function(){
-client.channels.cache.get("689394453877751835").bulkDelete(100).catch(console.error);
-console.log('DELETED last 100 messages in PÉNÜGY EADAT!');
-client.channels.cache.get("760790573354844190").send("`DELETED` last 100 messages in `PÉNÜGY EADAT!`");
-  }, 1000*60*60 ); // Hour 1
-  
-  setInterval(function(){
-client.channels.cache.get("689394488476827665").bulkDelete(100).catch(console.error);
-console.log('DELETED last 100 messages in ADÓ EADAT!');
-client.channels.cache.get("760790573354844190").send("`DELETED` last 100 messages in `ADÓ EADAT!`");
-  }, 1000*60*60 ); // Hour 1
-  
-  setInterval(function(){
-client.channels.cache.get("689376897309999113").bulkDelete(100).catch(console.error);
-console.log('DELETED last 100 messages in Közlemények!');
-client.channels.cache.get("760790573354844190").send("`DELETED` last 100 messages in `Közlemények!`");
-  }, 1000*60*120 ); // 1000 ms * 60s * 120/60min 
-  
-  client.user.setPresence({
-        status: "online",  //You can show online, idle....
-        game: {
-            name: "HIVATAL-os robot. Fejlesztés alatt by Gál András",  //The message shown
-            type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
-        }
-    });*/
-  
+ 
 });
 
 client.on("message", function(message) {
@@ -69,7 +37,7 @@ client.on("message", function(message) {
 		console.log('DELETED last 100 messages in PÉNÜGY EADAT!');
 		client.channels.cache.get("760790573354844190").send("`DELETED` last 100 messages in `PÉNÜGY EADAT!`");
 }else if (command === "adhelp") {
-		message.reply("`használd a következő admin paancsokat: `\n`Közlemény törlés: !delkoz`\n`Pénzügy eAdat törlése: !delpadat`\n`Adó eAdat törlése: !delaadat`");
+		message.reply("`használd a következő admin paancsokat: `\n`Közlemény törlés: !delkoz`\n`Pénzügy eAdat törlése: !delpadat`\n`Adó eAdat törlése: !delaadat`\n`Közlemények, Eadatok (mind) törlése: !delall`");
 }
 	else if (command === "delaadat") {
 		client.channels.cache.get("689394488476827665").bulkDelete(100).catch(console.error);
@@ -81,14 +49,16 @@ client.on("message", function(message) {
 	 client.channels.cache.get("689376897309999113").bulkDelete(100).catch(console.error);
 	console.log('DELETED last 100 messages in Közlemények!');
 	
-	/* var interval = setInterval (function () {
-            // use the message's channel (TextChannel) to send a new message
-            message.channel.bulkDelete(100)
-            .catch(console.error);
-			console.log('DEL! 100 message ACTIVATED again!');			// add error handling here
-        }, 1000*60*60 ); */
- } 
 
+else if (command === "delall") {
+	client.channels.cache.get("689376897309999113").bulkDelete(100).catch(console.error);
+	console.log('DELETED last 100 messages in Közlemények!');
+	client.channels.cache.get("689394488476827665").bulkDelete(100).catch(console.error);
+	console.log('DELETED last 100 messages in ADÓ EADAT!');
+	client.channels.cache.get("689394453877751835").bulkDelete(100).catch(console.error);
+	console.log('DELETED last 100 messages in PÉNÜGY EADAT!');
+	client.channels.cache.get("760790573354844190").send("`DELETED` last 100 messages in `Közlemények`, `ADÓ EADAT!`, `PÉNÜGY EADAT!`");
+}
 	else if (command === "help") {
 		message.reply("`használd a következő paancsokat: `\n`Válasz időt mutatja a bot és a géped között: !ping`\n`Számok gyors összeadására van: !szum`");
 }
